6fbb4aaac1996b0732a8db9c20aa30a24bcd70ac
sbx_user1051 <sbx_user1051@169.254.135.141>
2024-09-16 21:36:51 +0000

    Mon Sep 16 21:36:51 UTC 2024

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -1,7 +1,15 @@
+using BeFaster.App.Solutions.HLO;
+
 namespace BeFaster.App.Tests.Solutions.HLO;
 
 [TestFixture]
+[TestOf(typeof(HelloSolution))]
 public class HelloSolutionTest
 {
-    
+
+    [TestCase("test", ExpectedResult = "Hello World!")]
+    public string SayHello(string? friendName)
+    {
+        return HelloSolution.Hello(friendName);
+    }
 }
