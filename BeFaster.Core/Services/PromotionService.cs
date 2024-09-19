4fbab913a7288b743d81a5680400d1fe1366dd78
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 13:56:06 +0000

    Thu Sep 19 13:56:06 UTC 2024

--- BeFaster.Core/Services/PromotionService.cs
+++ BeFaster.Core/Services/PromotionService.cs
@@ -1,6 +1,30 @@
+using BeFaster.Core.Interfaces;
+using BeFaster.Domain.Entities;
+using BeFaster.Domain.Objects;
+using BeFaster.Domain.Repositories;
+
 namespace BeFaster.Core.Services;
 
-public class PromotionService
+public class PromotionService : IPromotionService
 {
+    private readonly IPromotionRepository<Promotion> _promotionRepository;
+    private readonly IPromotionFactory _promotionFactory;
+
+
+    public PromotionService(IPromotionRepository<Promotion> promotionRepository, IPromotionFactory promotionFactory)
+    {
+        _promotionRepository = promotionRepository;
+        _promotionFactory = promotionFactory;
+    }
     
+    // this service can handle multiple promotions per item and apply all promos at once. 
+    
+    //BUT in the future we would want more advanced logic for this service 
+    // some promotions are stackable, but some of them not
+    // some promos could potentially cancel another promotion, proobably even from another item
+    // anyway, i decided to keep it simple and not to overengineering here; 
+    public void ApplyPromotions(IEnumerable<ReceiptItem> receiptItems)
+    {
+        
+    }
 }
