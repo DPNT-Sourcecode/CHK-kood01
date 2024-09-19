09d665c1d0a655289ef6b0591b08367eaeb31a10
sbx_user1051 <sbx_user1051@169.254.95.245>
2024-09-19 18:43:58 +0000

    Thu Sep 19 18:43:58 UTC 2024

--- BeFaster.Core/Services/PromotionFactory/BuyXGetYFreePromo.cs
+++ BeFaster.Core/Services/PromotionFactory/BuyXGetYFreePromo.cs
@@ -18,8 +18,20 @@ public class BuyXGetYFreePromo : IPromo
         _promoPrice = promoPrice;
         _freeProductSku = freeProductSku;
     }
-    public int GetDiscount(Receipt receipt, char receiptId)
+    public int GetDiscount(Receipt receipt, char receiptKey)
     {
-        throw new NotImplementedException();
+        var item = receipt.GetItemByKey(receiptKey);
+        
+        if (item.BasketItem.Product.ProductSku != ProductSku) return 0;
+        
+        if(receipt)
+        
+        int quantity = item.BasketItem.Quantity;
+
+        if (quantity < _requiredQuantity) return 0;
+
+        int freeItemCount = quantity / _requiredQuantity;
+        
+        
     }
 }
