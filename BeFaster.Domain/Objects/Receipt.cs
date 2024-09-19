1b2d9568f9e54313bba9b1e34b991426826f5692
sbx_user1051 <sbx_user1051@169.254.95.245>
2024-09-19 18:07:58 +0000

    Thu Sep 19 18:07:58 UTC 2024

--- BeFaster.Domain/Objects/Receipt.cs
+++ BeFaster.Domain/Objects/Receipt.cs
@@ -3,21 +3,21 @@ namespace BeFaster.Domain.Objects;
 // I introduced this level of abstruction because at some point we would want to store and show some reciept specific info ,like reciept barcode, customer discounts, etc etc
 public class Receipt
 {
-    private IList<ReceiptItem> _receiptItems = new List<ReceiptItem>();
+    private readonly IDictionary<char, ReceiptItem> _receiptItems = new Dictionary<char, ReceiptItem>();
     
-    public IList<ReceiptItem> GetAllItems()
+    public IEnumerable<ReceiptItem> GetAllItems()
     {
-        return _receiptItems;
+        return _receiptItems.Select(s => s.Value);
     }
     
     public void AddItem(ReceiptItem item)
     {
-        _receiptItems.Add(item);
+        _receiptItems.TryAdd(item.BasketItem.Product.ProductSku,item);
     }
     
     public int CalculateTotal()
     {
-        return _receiptItems.Sum(item => item.Total);
+        return _receiptItems.Sum(item => item.Value.Total);
     }
     // in the future we want to add additional logic, such as: remove item, change item quantity, apply global promotions etc etc/
     
