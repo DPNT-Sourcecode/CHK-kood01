34e01647dcef762f4133df26a3530f9b569c77c9
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 16:08:07 +0000

    Thu Sep 19 16:08:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/CheckoutServiceTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/CheckoutServiceTest.cs
@@ -1,6 +1,88 @@
+using BeFaster.Core.Interfaces;
+using BeFaster.Core.Services;
+using BeFaster.Domain.Entities;
+using BeFaster.Domain.Models;
+using BeFaster.Domain.Objects;
+using BeFaster.Domain.Repositories;
+using Moq;
+
 namespace BeFaster.App.Tests.Solutions.Supermarket;
 
 public class CheckoutServiceTest
 {
+    private Mock<IProductRepository<Product>> _mockProductRepository;
+    private Mock<IPromotionService> _mockPromotionService;
+    private CheckoutService _checkoutService;
     
+    [SetUp]
+    public void setup()
+    {
+        _mockProductRepository = new Mock<IProductRepository<Product>>();
+        _mockPromotionService = new Mock<IPromotionService>();
+        _checkoutService = new CheckoutService(_mockProductRepository.Object, _mockPromotionService.Object);
+    }
+
+    [Test]
+    public void GetTotal_ValidProductSku_ReturnCorrectTotal()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'A', Price = 50 };
+        var productB = new Product { ProductSku = 'B', Price = 30 };
+        
+        _mockProductRepository.Setup(repo => repo.GetByProductSku('A')).Returns(productA);
+        _mockProductRepository.Setup(repo => repo.GetByProductSku('B')).Returns(productB);
+
+        var basket = new Basket();
+        basket.AddItem(productA);
+        basket.AddItem(productB);
+        var receipt = new Receipt();
+        receipt.AddItem(new ReceiptItem(new BasketItem(productA)));
+        receipt.AddItem(new ReceiptItem(new BasketItem(productB)));
+
+        _mockPromotionService.Setup(service => service.ApplyPromotions(It.IsAny<IEnumerable<ReceiptItem>>()));
+
+        var productInputModel = new ProductInputModel(new List<char> { 'A', 'B' });
+        
+        // Act
+        var total = _checkoutService.GetTotal(productInputModel);
+    
+        // Assert
+        Assert.That(total, Is.EqualTo(80));
+    }
+    [Test]
+    public void GetTotal_WithPromotions_AppliesPromotionsCorrectly()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'A', Price = 50 };
+        var productB = new Product { ProductSku = 'B', Price = 30 };
+        
+        _mockProductRepository.Setup(repo => repo.GetByProductSku('A')).Returns(productA);
+        _mockProductRepository.Setup(repo => repo.GetByProductSku('B')).Returns(productB);
+
+        var basket = new Basket();
+        basket.AddItem(productA);
+        basket.AddItem(productB);
+        var receipt = new Receipt();
+        receipt.AddItem(new ReceiptItem(new BasketItem(productA)));
+        receipt.AddItem(new ReceiptItem(new BasketItem(productB)));
+
+        //apply a promotion tat reduce the price by 10
+        _mockPromotionService.Setup(service => service.ApplyPromotions(It.IsAny<IEnumerable<ReceiptItem>>()))
+            .Callback((IEnumerable<ReceiptItem> receiptItems) =>
+            {
+                //assume promotion applies to product A
+                if (receiptItems.Count() > 0)
+                {
+                    receiptItems.First().ApplyPromotions(_=>40);
+                }
+            });
+
+        var productInputModel = new ProductInputModel(new List<char> { 'A', 'B' });
+        
+        // Act
+        var total = _checkoutService.GetTotal(productInputModel);
+        
+        // Assert
+        Assert.That(total, Is.EqualTo(70));
+    }
 }
