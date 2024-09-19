9785f8f434a10b3644cf01ac6c2285c6f0536138
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 12:26:06 +0000

    Thu Sep 19 12:26:06 UTC 2024

--- BeFaster.Domain/Entities/Promotion.cs
+++ BeFaster.Domain/Entities/Promotion.cs
@@ -1,6 +1,9 @@
 namespace BeFaster.Domain.Entities;
 
+// in real life implementation we will separate Entity from Objects, but for test task purpose we just keep it simple
 public class Promotion
 {
+    public int Id { get; set; }
+    public char ProductSku { get; set; }
     
 }
