409d0dbbbdd151bf9e22b8f61524210167bb7b71
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 12:41:06 +0000

    Thu Sep 19 12:41:06 UTC 2024

--- BeFaster.Domain/Models/ProductModel.cs
+++ BeFaster.Domain/Models/ProductModel.cs
@@ -1,6 +1,13 @@
 namespace BeFaster.Domain.Models;
 
+// we use this model to transfer data from customer faced layer to our service with business logic 
+// in a future it will pass some additional data, like cashier id, customer discount card, etc etc
 public class ProductModel
 {
-    
+    public char ProductSku { get; private set; }
+
+    public ProductModel(char productSku)
+    {
+        ProductSku = productSku;
+    }
 }
