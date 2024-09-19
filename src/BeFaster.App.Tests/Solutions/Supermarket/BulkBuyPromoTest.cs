483f131ca3c7bc801e774c58bf927117d953362f
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 15:20:07 +0000

    Thu Sep 19 15:20:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/BulkBuyPromoTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/BulkBuyPromoTest.cs
@@ -1,6 +1,72 @@
+using BeFaster.Core.Services.PromotionFactory;
+using BeFaster.Domain.Entities;
+using BeFaster.Domain.Objects;
+
 namespace BeFaster.App.Tests.Solutions.Supermarket;
 
 public class BulkBuyPromoTest
 {
+    [Test]
+    public void GetDiscount_EnoughQuantity_ReturnCorrectDiscount()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'A', Price = 50 };
+        var basketItem = new BasketItem(productA, 5);
+        var receiptItem = new ReceiptItem(basketItem);
+        var promo = new BulkBuyPromo('A', 3, 125);
+        
+        // Act 
+        var discount = promo.GetDiscount(receiptItem);
+
+        // Assert
+        Assert.That(discount, Is.EqualTo(25));
+    }
     
+    [Test]
+    public void GetDiscount_NotEnoughQuantity_ReturnsNoDiscount()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'A', Price = 50 };
+        var basketItem = new BasketItem(productA, 2);
+        var receiptItem = new ReceiptItem(basketItem);
+        var promo = new BulkBuyPromo('A', 3, 125);
+        
+        // Act 
+        var discount = promo.GetDiscount(receiptItem);
+        
+        // Assert
+        Assert.That(discount, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void GetDiscount_DifferentProductSku_ReturnsNoDiscount()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'B', Price = 50 };
+        var basketItem = new BasketItem(productA, 5);
+        var receiptItem = new ReceiptItem(basketItem);
+        var promo = new BulkBuyPromo('A', 3, 125);
+        
+        // Act 
+        var discount = promo.GetDiscount(receiptItem);
+        
+        // Assert
+        Assert.That(discount, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void GetDiscount_ZeroPricePerItem_ReturnsCorrectDiscount()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'B', Price = 0 };
+        var basketItem = new BasketItem(productA, 5);
+        var receiptItem = new ReceiptItem(basketItem);
+        var promo = new BulkBuyPromo('A', 3, 0);
+        
+        // Act 
+        var discount = promo.GetDiscount(receiptItem);
+        
+        // Assert
+        Assert.That(discount, Is.EqualTo(0));
+    }
 }
