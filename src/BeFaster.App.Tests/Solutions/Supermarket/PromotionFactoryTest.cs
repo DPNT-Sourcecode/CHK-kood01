f81080c1f4658a09c82a5f7b0a9a9c2196398f5a
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 15:26:07 +0000

    Thu Sep 19 15:26:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/PromotionFactoryTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/PromotionFactoryTest.cs
@@ -1,6 +1,65 @@
+using BeFaster.Core.Services.PromotionFactory;
+using BeFaster.Domain.Entities;
+
 namespace BeFaster.App.Tests.Solutions.Supermarket;
 
 public class PromotionFactoryTest
 {
+    private PromotionFactory _promotionFactory;
+    [SetUp]
+    public void SetUp()
+    {
+        _promotionFactory = new PromotionFactory();
+    }
+
+    [Test]
+    public void CreatePromotions_ValidPromotion_ReturnBulkBuyPromo()
+    {
+        // Arrange
+        var promotions = new List<Promotion>
+        {
+            
+        }
+        
+        // Act
+        
+        
+        // Assert
+    } 
     
+    [Test]
+    public void CreatePromotions_EmptyPromotionList_ReturnEmptyList()
+    {
+        // Arrange
+        
+        
+        // Act
+        
+        
+        // Assert
+    } 
+    
+    [Test]
+    public void CreatePromotions_NullPromotionsList_ThrowsArgumentNullException()
+    {
+        // Arrange
+        
+        
+        // Act
+        
+        
+        // Assert
+    } 
+    
+    [Test]
+    public void CreatePromotions_MultipleValidPromotions_ReturnCorrectPromos()
+    {
+        // Arrange
+        
+        
+        // Act
+        
+        
+        // Assert
+    } 
 }
