9a4fdb7b066520f18d237b3dc46ea8284aac3b6a
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 15:08:07 +0000

    Thu Sep 19 15:08:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/InputProcessingServiceTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/InputProcessingServiceTest.cs
@@ -1,10 +1,41 @@
+using BeFaster.Core.Interfaces;
+using BeFaster.Core.Services;
+using BeFaster.Domain.Models;
+
 namespace BeFaster.App.Tests.Solutions.Supermarket;
 
 public class InputProcessingServiceTest
 {
+    private IInputProcessingService _inputProcessingService;
+    
     [SetUp]
     public void SetUp()
     {
+        _inputProcessingService = new InputProcessingService();
+    }
+
+    [Test]
+    public void ProcessInput_ShouldReturnProductInputModel_WhenInputIsValid()
+    {
+        // Arrange
+        var input = "abc";
         
+        // Act
+        var result = _inputProcessingService.ProcessInput(input);
+        
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOf<ProductInputModel>(result);
+        CollectionAssert.AreEqual(new List<char> { 'A', 'B', 'C' }, result.ProductSkuList);
+    }
+    
+    [Test]
+    public void ProcessInput_ShouldThrowArgumentException_WhenInputContainsInvalidCharacters()
+    {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 }
