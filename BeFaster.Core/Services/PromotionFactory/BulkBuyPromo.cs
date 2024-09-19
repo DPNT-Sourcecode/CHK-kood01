1e550232ceb883d3f79d3e4d6510600197316062
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 13:44:06 +0000

    Thu Sep 19 13:44:06 UTC 2024

--- BeFaster.Core/Services/PromotionFactory/BulkBuyPromo.cs
+++ BeFaster.Core/Services/PromotionFactory/BulkBuyPromo.cs
@@ -1,6 +1,17 @@
+using BeFaster.Core.Interfaces;
+using BeFaster.Domain.Objects;
+
 namespace BeFaster.Core.Services.PromotionFactory;
 
-public class BulkPromo
+public class BulkBuyPromo : IPromo
 {
-    
+    public char ProductSku { get; }
+    public int GetDiscount(ReceiptItem item)
+    {
+        if (item.BasketItem.Product.ProductSku != ProductSku) return 0;
+
+        int quantity = item.BasketItem.Quantity;
+        int pricePerItem = item.BasketItem.Product.Price;
+        
+    }
 }
