ef0cbc802174ea3d278a27dbdc6dbbf4c3ca6459
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 15:41:07 +0000

    Thu Sep 19 15:41:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/PromoServiceTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/PromoServiceTest.cs
@@ -1,6 +1,24 @@
+using BeFaster.Core.Interfaces;
+using BeFaster.Core.Services;
+using BeFaster.Domain.Entities;
+using BeFaster.Domain.Repositories;
+using Moq;
+
 namespace BeFaster.App.Tests.Solutions.Supermarket;
 
 public class PromoServiceTest
 {
+    private Mock<IPromotionRepository<Promotion>> _mockPromotionRepository;
+    private Mock<IPromotionFactory> _mockPromotionFactory;
+    private PromotionService _promotionService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockPromotionRepository = new Mock<IPromotionRepository<Promotion>>();
+        _mockPromotionFactory = new Mock<IPromotionFactory>();
+        _promotionService = new PromotionService(_mockPromotionRepository.Object, _mockPromotionFactory.Object);
+    }
     
+    []
 }
