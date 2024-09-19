d5e8b51eac3533fa9361c091b26f5aa0d73d41cc
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 13:38:06 +0000

    Thu Sep 19 13:38:06 UTC 2024

--- BeFaster.Domain/Models/ProductInputModel.cs
+++ BeFaster.Domain/Models/ProductInputModel.cs
@@ -2,11 +2,11 @@ namespace BeFaster.Domain.Models;
 
 // we use this model to transfer data from customer faced layer to our service with business logic 
 // in a future it will pass some additional data, like cashier id, customer discount card, etc etc
-public class InputModel
+public class ProductInputModel
 {
     public IList<char> ProductSkuList { get; private set; }
 
-    public InputModel(IList<char> productSkuList)
+    public ProductInputModel(IList<char> productSkuList)
     {
         ProductSkuList = productSkuList;
     }
