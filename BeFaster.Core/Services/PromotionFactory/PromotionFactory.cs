5ff71bfc85fb8cdbe7acce191e1c2b514bb9a70f
sbx_user1051 <sbx_user1051@169.254.130.253>
2024-09-19 21:20:58 +0000

    Thu Sep 19 21:20:58 UTC 2024

--- BeFaster.Core/Services/PromotionFactory/PromotionFactory.cs
+++ BeFaster.Core/Services/PromotionFactory/PromotionFactory.cs
@@ -9,40 +9,27 @@ public class PromotionFactory : IPromotionFactory
     {
         if (promotionEntities == null)
             return Enumerable.Empty<IPromo>();
-        
+
         // Group promotions by SKU
 
         var promotionsGrouped = promotionEntities
             .GroupBy(p => p.ProductSku)
-            .ToDictionary(g => g.Key, g => g.ToList()); 
-        
+            .ToDictionary(g => g.Key, g => g.ToList());
+
         var bulkPromos = promotionsGrouped.Select(g =>
         {
             var promotions = g.Value;
 
             promotions.Where(p => p.FreeProductSku.HasValue).Select(entity =>
             {
-                return new BuyXGetYFreePromo(entity.ProductSku, entity.RequiredQuantity, entity.PromoPrice,entity.FreeProductSku.Value);
+                return new BuyXGetYFreePromo(entity.ProductSku, entity.RequiredQuantity, entity.PromoPrice,
+                    entity.FreeProductSku.Value);
             });
 
             promotions.Where(p => !p.FreeProductSku.HasValue).Select(entity =>
             {
                 return new BulkBuyPromo(entity.ProductSku, entity.RequiredQuantity, entity.PromoPrice);
             });
-        }
-            
-            
-            // we can do the same with Type field, as i described in Promotion Entity class, i decided to keep it simple for now
-            
-            
-            if (entity.FreeProductSku.HasValue)
-            {
-                return new BuyXGetYFreePromo(entity.ProductSku, entity.RequiredQuantity, entity.PromoPrice,entity.FreeProductSku.Value);
-            }
-            else
-            {
-                
-            }
         });
         
         return bulkPromos;
