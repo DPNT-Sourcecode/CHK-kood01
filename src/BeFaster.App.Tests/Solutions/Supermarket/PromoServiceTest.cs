59e2012c8b45c289aefba7701e341e4fba520b79
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 15:47:07 +0000

    Thu Sep 19 15:47:07 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/PromoServiceTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/PromoServiceTest.cs
@@ -1,6 +1,8 @@
 using BeFaster.Core.Interfaces;
 using BeFaster.Core.Services;
+using BeFaster.Core.Services.PromotionFactory;
 using BeFaster.Domain.Entities;
+using BeFaster.Domain.Objects;
 using BeFaster.Domain.Repositories;
 using Moq;
 
@@ -19,6 +21,31 @@ public class PromoServiceTest
         _mockPromotionFactory = new Mock<IPromotionFactory>();
         _promotionService = new PromotionService(_mockPromotionRepository.Object, _mockPromotionFactory.Object);
     }
-    
-    []
+
+    [Test]
+    public void ApplyPromotions_ValidPromotions_ApploesCorrectDiscounts()
+    {
+        // Arrange
+        var productA = new Product { ProductSku = 'A', Price = 50 };
+        var basketItem = new BasketItem(productA, 3);
+        var receiptItem = new ReceiptItem(basketItem);
+
+        var promotions = new List<IPromo>
+        {
+            new BulkBuyPromo('A', 3, 125)
+        };
+        var promotionEntities = new List<Promotion>
+        {
+            new Promotion { Id = 1, ProductSku = 'A', RequiredQuantity = 3, PromoPrice = 125 }
+        };
+
+        _mockPromotionRepository.Setup(repo => repo.GetAll()).Returns(promotionEntities);
+        _mockPromotionFactory.Setup(factory => factory.CreatePromotions(promotionEntities)).Returns(promotions);
+        
+        // Act
+        _promotionService.ApplyPromotions(new List<ReceiptItem> {receiptItem});
+        
+        // Assert
+        Assert.That(receiptItem.Total, Is.EqualTo(125));
+    }
 }
