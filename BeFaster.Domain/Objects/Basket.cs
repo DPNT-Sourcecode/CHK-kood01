935a65348226f4ba0b78610041ccc417b57a4656
sbx_user1051 <sbx_user1051@169.254.35.157>
2024-09-19 12:47:06 +0000

    Thu Sep 19 12:47:06 UTC 2024

--- BeFaster.Domain/Objects/Basket.cs
+++ BeFaster.Domain/Objects/Basket.cs
@@ -2,5 +2,12 @@ namespace BeFaster.Domain.Objects;
 
 public class Basket
 {
+    private Dictionary<char, BasketItem> _items = new();
+    
+    // i need to add items
+    
+    // retriev items 
+    
+    // maybe calculate price on basket level? 
     
 }
--- BeFaster.Domain/Objects/BasketItem.cs
+++ BeFaster.Domain/Objects/BasketItem.cs
@@ -1,6 +1,21 @@
+using BeFaster.Domain.Entities;
+
 namespace BeFaster.Domain.Objects;
 
 public class BasketItem
 {
-    
+    public Product Product { get; private set; }
+    public int Quantity { get; private set; }
+    public int PlainAmount => Product.Price * Quantity;
+
+    public BasketItem(Product product, int qty = 1)
+    {
+        Product = product;
+        Quantity = qty;
+    }
+
+    public void AddItem()
+    {
+        Quantity += 1;
+    }
 }
