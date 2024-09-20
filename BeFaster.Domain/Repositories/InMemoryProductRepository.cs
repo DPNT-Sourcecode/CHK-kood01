5a2432202e6fb46aa053e6bd2a62363d37950406
sbx_user1051 <sbx_user1051@169.254.39.165>
2024-09-20 12:48:46 +0000

    Fri Sep 20 12:48:46 UTC 2024

--- BeFaster.Domain/Repositories/InMemoryProductRepository.cs
+++ BeFaster.Domain/Repositories/InMemoryProductRepository.cs
@@ -13,15 +13,25 @@ public class InMemoryProductRepository : IProductRepository<Product>
         { 'E', new Product { ProductSku = 'E', Price = 40 } },
         { 'F', new Product { ProductSku = 'F', Price = 10 } },
         { 'G', new Product { ProductSku = 'G', Price = 20 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
-        { 'F', new Product { ProductSku = 'F', Price = 10 } },
+        { 'H', new Product { ProductSku = 'H', Price = 10 } },
+        { 'I', new Product { ProductSku = 'I', Price = 35 } },
+        { 'J', new Product { ProductSku = 'J', Price = 60 } },
+        { 'K', new Product { ProductSku = 'K', Price = 80 } },
+        { 'L', new Product { ProductSku = 'L', Price = 90 } },
+        { 'M', new Product { ProductSku = 'M', Price = 15 } },
+        { 'N', new Product { ProductSku = 'N', Price = 40 } },
+        { 'O', new Product { ProductSku = 'O', Price = 10 } },
+        { 'P', new Product { ProductSku = 'P', Price = 50 } },
+        { 'Q', new Product { ProductSku = 'Q', Price = 30 } },
+        { 'R', new Product { ProductSku = 'R', Price = 50 } },
+        { 'S', new Product { ProductSku = 'S', Price = 30 } },
+        { 'T', new Product { ProductSku = 'T', Price = 20 } },
+        { 'U', new Product { ProductSku = 'U', Price = 40 } },
+        { 'V', new Product { ProductSku = 'V', Price = 50 } },
+        { 'W', new Product { ProductSku = 'W', Price = 20 } },
+        { 'X', new Product { ProductSku = 'X', Price = 90 } },
+        { 'Y', new Product { ProductSku = 'Y', Price = 10 } },
+        { 'Z', new Product { ProductSku = 'Z', Price = 50 } },
     };
     
     Our price table and offers: 
@@ -69,3 +79,4 @@ public class InMemoryProductRepository : IProductRepository<Product>
         }
     }
 }
+
