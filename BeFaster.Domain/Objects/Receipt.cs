44678792d2417fcc97c0dea9d045b696fce492de
sbx_user1051 <sbx_user1051@169.254.58.53>
2024-09-21 17:50:24 +0000

    Sat Sep 21 17:50:24 UTC 2024

--- BeFaster.Domain/Objects/Receipt.cs
+++ BeFaster.Domain/Objects/Receipt.cs
@@ -23,7 +23,7 @@ public class Receipt
     
     public int CalculateTotal()
     {
-        return (int)ReceiptItems.Sum(s => s.DiscountedTotal);
+        return (int)Math.Round(ReceiptItems.Sum(s => s.DiscountedTotal),2,MidpointRounding.AwayFromZero);
     }
     // in the future we want to add additional logic, such as: remove item, change item quantity, apply global promotions etc etc/
     
