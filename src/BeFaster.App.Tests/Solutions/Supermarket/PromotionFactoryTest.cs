647ef362dc3115c8a9adc0ddea96d47e70dc221c
sbx_user1051 <sbx_user1051@169.254.58.53>
2024-09-21 17:16:42 +0000

    Sat Sep 21 17:16:42 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/PromotionFactoryTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/PromotionFactoryTest.cs
@@ -7,7 +7,7 @@ namespace BeFaster.App.Tests.Solutions.Supermarket;
 public class PromotionFactoryTest
 {
     private PromotionFactory _promotionFactory;
-    
+
     [SetUp]
     public void SetUp()
     {
@@ -20,10 +20,12 @@ public class PromotionFactoryTest
         // Arrange
         var promotions = new List<Promotion>
         {
-            new Promotion {Type = PromotionType.BuyXGetYFree, ProductSkus = ['A'], RequiredQuantity = 2, FreeProductSku = 'B'},
-            new Promotion {Type = PromotionType.BuyXGetYFree, ProductSkus = ['C'], RequiredQuantity = 2, PromoPrice = 50m},
+            new Promotion
+                { Type = PromotionType.BuyXGetYFree, ProductSkus = ['A'], RequiredQuantity = 2, FreeProductSku = 'B' },
+            new Promotion
+                { Type = PromotionType.BuyXGetYFree, ProductSkus = ['C'], RequiredQuantity = 2, PromoPrice = 50m },
         };
-        
+
         // Act
         var result = _promotionFactory.CreatePromotions(promotions).ToList();
 
@@ -32,17 +34,19 @@ public class PromotionFactoryTest
         Assert.IsInstanceOf<BuyXGetYFreePromo>(result[0]);
         Assert.IsInstanceOf<BuyXGetYFreePromo>(result[1]);
     }
-    
+
     [Test]
     public void CreatePromotions_ShouldReturnPromotionsInCorrectOrder()
     {
         // Arrange
         var promotions = new List<Promotion>
         {
-            new Promotion {Type = PromotionType.BuyXGetYFree, ProductSkus = ['C'], RequiredQuantity = 2, PromoPrice = 50m},
-            new Promotion {Type = PromotionType.BuyXGetYFree, ProductSkus = ['A'], RequiredQuantity = 2, FreeProductSku = 'B'},
+            new Promotion
+                { Type = PromotionType.BuyXGetYFree, ProductSkus = ['C'], RequiredQuantity = 2, PromoPrice = 50m },
+            new Promotion
+                { Type = PromotionType.BuyXGetYFree, ProductSkus = ['A'], RequiredQuantity = 2, FreeProductSku = 'B' },
         };
-        
+
         // Act
         var result = _promotionFactory.CreatePromotions(promotions).ToList();
 
@@ -51,13 +55,13 @@ public class PromotionFactoryTest
         Assert.IsInstanceOf<BuyXGetYFreePromo>(result[0]);
         Assert.IsInstanceOf<BuyXGetYFreePromo>(result[1]);
     }
-    
+
     [Test]
     public void CreatePromotions_ShouldReturnEmptyResult_WhenGivenEmptyInput()
     {
         // Arrange
         var promotions = new List<Promotion>();
-        
+
         // Act
         var result = _promotionFactory.CreatePromotions(promotions).ToList();
 
@@ -65,7 +69,7 @@ public class PromotionFactoryTest
         Assert.IsNotNull(result);
         Assert.IsEmpty(result);
     }
-    
+
     [Test]
     public void CreatePromotions_ShouldReturnEmptyResult_WhenGivenNullInput()
     {
@@ -76,73 +80,4 @@ public class PromotionFactoryTest
         Assert.IsNotNull(result);
         Assert.IsEmpty(result);
     }
-
-
-    // [Test]
-    // public void CreatePromotions_ValidPromotion_ReturnBulkBuyPromo()
-    // {
-    //     // Arrange
-    //     var promotions = new List<Promotion>
-    //     {
-    //         new Promotion { ProductSku = 'A', RequiredQuantity = 3, PromoPrice = 125, Type = PromotionType.BulkBuy}
-    //     };
-    //
-    //     // Act
-    //     var result = _promotionFactory.CreatePromotions(promotions);
-    //
-    //     // Assert
-    //     Assert.IsNotNull(result);
-    //     Assert.IsInstanceOf<BulkBuyPromo>(result.FirstOrDefault());
-    //
-    //     var bulkBuyPromo = result.FirstOrDefault() as BulkBuyPromo;
-    //     Assert.That(bulkBuyPromo.ProductSkus, Is.EqualTo('A'));
-    // } 
-    //
-    // [Test]
-    // public void CreatePromotions_EmptyPromotionList_ReturnEmptyList()
-    // {
-    //     // Arrange
-    //     var promotions = new List<Promotion>();
-    //
-    //     // Act
-    //     var result = _promotionFactory.CreatePromotions(promotions);
-    //
-    //     // Assert
-    //     Assert.IsNotNull(result);
-    //     Assert.IsEmpty(result);
-    // } 
-    //
-    // [Test]
-    // public void CreatePromotions_NullPromotionsList_ReturnNotNull()
-    // {
-    //     // Act 
-    //     var promotioms = _promotionFactory.CreatePromotions(null);
-    //     
-    //     // Assert
-    //     Assert.IsNotNull(promotioms);
-    // } 
-    //
-    // [Test]
-    // public void CreatePromotions_MultipleValidPromotions_ReturnCorrectPromos()
-    // {
-    //     // Arrange
-    //     var promotions = new List<Promotion>
-    //     {
-    //         new Promotion { ProductSku = 'A', RequiredQuantity = 3, PromoPrice = 125, Type = PromotionType.BulkBuy},
-    //         new Promotion { ProductSku = 'B', RequiredQuantity = 2, PromoPrice = 70, Type = PromotionType.BulkBuy}
-    //     };
-    //
-    //     // Act
-    //     var result = _promotionFactory.CreatePromotions(promotions);
-    //     
-    //     // Assert
-    //     Assert.IsNotNull(result);
-    //     Assert.That(result.Count(), Is.EqualTo(2));
-    //
-    //     var promo1 = result.ElementAt(0) as BulkBuyPromo;
-    //     var promo2 = result.ElementAt(1) as BulkBuyPromo;
-    //
-    //     Assert.AreEqual('A', promo1.ProductSkus);
-    //     Assert.AreEqual('B', promo2.ProductSkus);
-    // } 
 }
