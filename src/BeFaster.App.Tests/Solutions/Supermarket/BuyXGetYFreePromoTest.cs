8e5001238d780df279d35d14e9d2b5a237639d73
sbx_user1051 <sbx_user1051@169.254.58.53>
2024-09-21 17:04:42 +0000

    Sat Sep 21 17:04:42 UTC 2024

--- src/BeFaster.App.Tests/Solutions/Supermarket/BuyXGetYFreePromoTest.cs
+++ src/BeFaster.App.Tests/Solutions/Supermarket/BuyXGetYFreePromoTest.cs
@@ -1,51 +1,80 @@
-// using BeFaster.Core.Services.PromotionFactory;
-// using BeFaster.Domain.Entities;
-// using BeFaster.Domain.Objects;
-//
-// [TestFixture]
-// public class BuyXGetYFreePromoTests
-// {
-//     private BuyXGetYFreePromo _promo;
-//
-//     [SetUp]
-//     public void SetUp()
-//     {
-//         _promo = new BuyXGetYFreePromo('A', new List<Promotion>{new Promotion {ProductSku = 'A', RequiredQuantity = 3, FreeProductSku = 'B'}});
-//     }
-//
-//     [Test]
-//     public void GetDiscount_ShouldReturnZero_WhenReceiptDoesNotContainProduct()
-//     {
-//         // Arrange 
-//         var receipt = new Receipt();
-//
-//         // Act
-//         var discount = _promo.GetDiscount(receipt, 'A');
-//
-//         // Assert
-//         Assert.AreEqual(0, discount);
-//     }
-//
-//     [Test]
-//     public void GetDiscount_ShouldApplyPromotion_WhenEligibleForFreeItem()
-//     {
-//         // Arrange
-//         var receipt = new Receipt();
-//         var basketItemA = new BasketItem( new Product { ProductSku = 'A', Price = 50 }, 6); 
-//         var basketItemB = new BasketItem( new Product { ProductSku = 'B', Price = 10 }, 2); 
-//
-//         var receiptItemA = new ReceiptItem(basketItemA);
-//         var receiptItemB = new ReceiptItem(basketItemB);
-//
-//         receipt.AddItem(receiptItemA);
-//         receipt.AddItem(receiptItemB);
-//
-//         // Act
-//         var discount = _promo.GetDiscount(receipt, 'A');
-//
-//         // Assert
-//         Assert.AreEqual(0, discount); 
-//         Assert.AreEqual(0, receiptItemB.Total); 
-//         Assert.AreEqual(300, receiptItemA.Total);
-//     }
-// }
+using BeFaster.Core.Services.PromotionFactory;
+using BeFaster.Domain.Entities;
+using BeFaster.Domain.Objects;
+
+[TestFixture]
+public class BuyXGetYFreePromoTests
+{
+    private BuyXGetYFreePromo _promo;
+    private Receipt _receipt;
+
+    [SetUp]
+    public void Setup()
+    {
+        List<char> productSkus = ['A', 'B'];
+        char? freeProductSku = 'C';
+        int requiredQuantity = 3;
+
+        _promo = new BuyXGetYFreePromo(productSkus, requiredQuantity, freeProductSku);
+        _receipt = new Receipt();
+    }
+
+    [Test]
+    public void ApplyDiscounts_ShouldApplyPromo_WhenEligableForFreeItem()
+    {
+        // Arrange 
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'C', Total = 50m, DiscountedTotal = 50m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'C', Total = 50m, DiscountedTotal = 50m, AppliedPromo = null});
+        
+        // Act
+        _promo.ApplyDiscount(_receipt);
+        
+        // Assert 
+        var freeItems = _receipt.ReceiptItems.Where(item => item.ProductSku == 'C' && item.AppliedPromo == _promo).ToList();
+
+        Assert.That(freeItems.Count, Is.EqualTo(1));
+        Assert.IsTrue(freeItems.All(item => item.DiscountedTotal == 0m));
+    }
+    
+    [Test]
+    public void ApplyDiscounts_ShouldApplyPromoForMultipleTimeProducts_WhenFreeItemsTwiceMoreFromRequiredQuantity()
+    {
+        // Arrange 
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'C', Total = 50m, DiscountedTotal = 50m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'C', Total = 50m, DiscountedTotal = 50m, AppliedPromo = null});
+        
+        // Act
+        _promo.ApplyDiscount(_receipt);
+        
+        // Assert 
+        var freeItems = _receipt.ReceiptItems.Where(item => item.ProductSku == 'C' && item.AppliedPromo == _promo).ToList();
+
+        Assert.That(freeItems.Count, Is.EqualTo(2));
+        Assert.IsTrue(freeItems.All(item => item.DiscountedTotal == 0m));
+    }
+    
+    [Test]
+    public void ApplyDiscounts_ShouldNotApplyPromo_WhenNotEnoughItemsInBasket()
+    {
+        // Arrange 
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'A', Total = 50m, DiscountedTotal = 100m, AppliedPromo = null});
+        _receipt.ReceiptItems.Add(new ReceiptItem{ProductSku = 'C', Total = 50m, DiscountedTotal = 50m, AppliedPromo = null});
+        
+        // Act
+        _promo.ApplyDiscount(_receipt);
+        
+        // Assert 
+        var freeItems = _receipt.ReceiptItems.Where(item => item.ProductSku == 'C' && item.AppliedPromo == _promo).ToList();
+
+        Assert.That(freeItems.Count, Is.EqualTo(0));
+    }
+}
