f5e17d76dad764157bc7d09c34c845be540fd798
sbx_user1051 <sbx_user1051@169.254.58.53>
2024-09-21 16:25:42 +0000

    Sat Sep 21 16:25:42 UTC 2024

--- BeFaster.Domain/Repositories/InMemoryPromotionRepository.cs
+++ BeFaster.Domain/Repositories/InMemoryPromotionRepository.cs
@@ -7,21 +7,21 @@ public class InMemoryPromotionRepository : IPromotionRepository<Promotion>
 {
     private readonly List<Promotion> _promotions = new()
     {
-        new Promotion {ProductSku = 'A',RequiredQuantity = 3,PromoPrice = 130, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'A',RequiredQuantity = 5,PromoPrice = 200, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'B',RequiredQuantity = 2,PromoPrice = 45, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'E',RequiredQuantity = 2, FreeProductSku = 'B', Type = PromotionType.BuyXGetYFree}, // 2E get one B free
-        new Promotion {ProductSku = 'F',RequiredQuantity = 3, FreeProductSku = 'F', Type = PromotionType.BuyXGetYFree},  // 2F get one F free   |YOU have to have at least 3 items in a basket
-        new Promotion {ProductSku = 'H',RequiredQuantity = 5, PromoPrice = 45, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'H',RequiredQuantity = 10, PromoPrice = 80, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'K',RequiredQuantity = 2, PromoPrice = 150, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'N',RequiredQuantity = 3, FreeProductSku = 'M', Type = PromotionType.BuyXGetYFree},
-        new Promotion {ProductSku = 'P',RequiredQuantity = 5, PromoPrice = 200, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'Q',RequiredQuantity = 3, PromoPrice = 80, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'R',RequiredQuantity = 3, FreeProductSku = 'Q', Type = PromotionType.BuyXGetYFree},
-        new Promotion {ProductSku = 'U',RequiredQuantity = 4, FreeProductSku = 'U', Type = PromotionType.BuyXGetYFree},
-        new Promotion {ProductSku = 'V',RequiredQuantity = 2, PromoPrice = 90, Type = PromotionType.BulkBuy},
-        new Promotion {ProductSku = 'V',RequiredQuantity = 3, PromoPrice = 130, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['A'],RequiredQuantity = 3,PromoPrice = 130, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['A'],RequiredQuantity = 5,PromoPrice = 200, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['B'],RequiredQuantity = 2,PromoPrice = 45, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['E'],RequiredQuantity = 2, FreeProductSku = 'B', Type = PromotionType.BuyXGetYFree}, // 2E get one B free
+        new Promotion {ProductSkus = ['F'],RequiredQuantity = 3, FreeProductSku = 'F', Type = PromotionType.BuyXGetYFree},  // 2F get one F free   |YOU have to have at least 3 items in a basket
+        new Promotion {ProductSkus = ['H'],RequiredQuantity = 5, PromoPrice = 45, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['H'],RequiredQuantity = 10, PromoPrice = 80, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['k'],RequiredQuantity = 2, PromoPrice = 150, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['N'],RequiredQuantity = 3, FreeProductSku = 'M', Type = PromotionType.BuyXGetYFree},
+        new Promotion {ProductSkus = ['P'],RequiredQuantity = 5, PromoPrice = 200, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['Q'],RequiredQuantity = 3, PromoPrice = 80, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['R'],RequiredQuantity = 3, FreeProductSku = 'Q', Type = PromotionType.BuyXGetYFree},
+        new Promotion {ProductSkus = ['U'],RequiredQuantity = 4, FreeProductSku = 'U', Type = PromotionType.BuyXGetYFree},
+        new Promotion {ProductSkus = ['V'],RequiredQuantity = 2, PromoPrice = 90, Type = PromotionType.BulkBuy},
+        new Promotion {ProductSkus = ['V'],RequiredQuantity = 3, PromoPrice = 130, Type = PromotionType.BulkBuy},
     };
     public IEnumerable<Promotion> GetAll()
     {
